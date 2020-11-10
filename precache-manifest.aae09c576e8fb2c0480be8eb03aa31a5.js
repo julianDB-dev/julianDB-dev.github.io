07d69d862efa93f44f41947f3507920b51878ca4
Del Bosque Julian <julian.delbosque@yahoo.fr>
2020-11-10 18:16:43 +0100

    init

---
+++ precache-manifest.aae09c576e8fb2c0480be8eb03aa31a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd02a960c9b47c94f5d6dfa2e4b4497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85a52df5d562cc0804ca",
+    "url": "/static/css/main.752493e2.chunk.css"
+  },
+  {
+    "revision": "96c7513ecbd274512823",
+    "url": "/static/js/2.e2079494.chunk.js"
+  },
+  {
+    "revision": "923a93445da4455d8e7e236ec4fa5442",
+    "url": "/static/js/2.e2079494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a52df5d562cc0804ca",
+    "url": "/static/js/main.315fec76.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
